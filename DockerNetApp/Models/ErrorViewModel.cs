f1acccec2cbfe8614904678ca69474de3792c26d
Carlo Palladino”git config --global user.name CarlosPalladino” <carlosezequiel19palladino@gmail.com>
2023-11-14 08:06:21 -0300

    Agregar archivos de proyecto.

---
+++ DockerNetApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DockerNetApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
